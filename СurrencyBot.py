e3823bc3721498bbebdcac416d84a0f6ff101c85
kvrsss <petr.famusov.01@mail.ru>
2025-01-23 16:44:12 +0300

    Добавлены вступительное сообщение и список доступных валют

--- СurrencyBot.py
+++ СurrencyBot.py
@@ -2,3 +2,23 @@ import telebot
 
 
 TOKEN = '7209060346:AAGWbVLkT8t03m-hPQlYx9ri9YraI22oobU'
+
+bot = telebot.TeleBot(TOKEN)
+
+keys = {'биткоин': 'BTC', 'эфириум': 'ETH', 'доллар': 'USD'}
+
+
+@bot.message_handler(commands=['start', 'help'])
+def help(message: telebot.types.Message):
+    text = ('Чтобы начать введите:\n<имя валюты> <в какую валюты перевести> <количество переводимой валюты>\n'
+            'Увидеть список всех доступных валют: /values')
+    bot.reply_to(message, text)
+
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = 'Доступные валюты: '
+    for k in keys:
+        text = '\n'.join((text, k))
+    bot.reply_to(message, text)
+
+bot.polling()
